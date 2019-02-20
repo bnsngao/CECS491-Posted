4f6ba53afd105567447ba6de764e5b7f37cdd37a
nickamcd <37389764+nickamcd@users.noreply.github.com>
2019-02-19 17:12:41 -0800

    Added RecyclerView

---
+++ Posted/app/src/main/java/com/example/posted/Adapter.java
@@ -0,0 +1,58 @@
+package com.example.posted;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.RatingBar;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.myViewHolder> {
+
+    Context mContext;
+    List<ProfileItem> mData;
+
+    public Adapter(Context mContext, List<ProfileItem> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v = inflater.inflate(R.layout.profile_item_view, viewGroup, false);
+        return new myViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        holder.profilePhoto.setImageResource(mData.get(position).getProfilePhoto());
+        holder.profileName.setText(mData.get(position).getProfileName());
+        holder.profileRating.setNumStars(mData.get(position).ratingNumber);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class myViewHolder extends RecyclerView.ViewHolder {
+        ImageView profilePhoto;
+        TextView profileName;
+        RatingBar profileRating;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profilePhoto = itemView.findViewById(R.id.profile_picture);
+            profileName = itemView.findViewById(R.id.txt_username);
+            profileRating = itemView.findViewById(R.id.ratingBar);
+        }
+    }
+}
--- Posted/app/src/main/java/com/example/posted/MainActivity.java
+++ Posted/app/src/main/java/com/example/posted/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.posted;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<ProfileItem> mList = new ArrayList<>();
+        mList.add(new ProfileItem());
+        mList.add(new ProfileItem());
+        mList.add(new ProfileItem());
+        mList.add(new ProfileItem());
+
+
+        //set up recycler view with adapter
+        RecyclerView recyclerView = findViewById(R.id.rv_profile_list);
+        Adapter adapter = new Adapter(this, mList);
     }
 }
