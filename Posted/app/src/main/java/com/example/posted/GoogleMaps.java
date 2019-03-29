6d3ea9041642201dd068a8b844cfbb5a38e174e1
Julian Tran <juliantran097@gmail.com>
2019-03-28 23:33:21 -0700

    google maps api
    implemented a google maps to the application. user can search for a location and it will move and mark it on the map.
    to do: open google map location of a passed in location given by the tourist/guide

---
+++ Posted/app/src/main/java/com/example/posted/GoogleMaps.java
@@ -0,0 +1,69 @@
+package com.example.posted;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.SearchView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    SupportMapFragment mapFragment;
+    SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.google_maps);
+
+        searchView = findViewById(R.id.sv_location);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null || !location.equals(""))
+                {
+                    Geocoder geocoder = new Geocoder(GoogleMaps.this);
+                    try{
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    map.addMarker(new MarkerOptions().position(latLng).title(location));
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+    }
+}
