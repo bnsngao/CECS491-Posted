23af5dc945b4c19a330fa61d2da4a5c9c8ed6c21
Julian Tran <juliantran097@gmail.com>
2019-05-07 17:11:24 -0700

    Open in Google Maps
    Should now open the location directly into Google Maps instead of Android Studio Maps. Just need to link the "open in google maps" button to open up this activity whenever ready

--- Posted/app/src/main/java/com/example/posted/GoogleMaps.java
+++ Posted/app/src/main/java/com/example/posted/GoogleMaps.java
@@ -7,18 +7,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback{
+public class GoogleMaps extends FragmentActivity{
 
     GoogleMap map;
 
@@ -26,17 +21,9 @@ public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback{
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.google_map);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        double coLat = 33.7897058; //placeholder coordinates (pass in Ben's coordinates)
-        double coLong = -118.1411507;
+        double coLat = 33.93296348731534; //placeholder coordinates (pass in Ben's coordinates)
+        double coLong = -118.11800479888916; //current coordinates for raising canes in Downey
 
         Geocoder geocoder;
         List<Address> addresses = null;
@@ -48,6 +35,7 @@ public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback{
             e.printStackTrace();
         }
 
+        //convert coordinates into address
         String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
         String city = addresses.get(0).getLocality();
         String state = addresses.get(0).getAdminArea();
@@ -55,9 +43,16 @@ public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback{
         String postalCode = addresses.get(0).getPostalCode();
         String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
 
-        LatLng coordinates = new LatLng(coLat, coLong);
-        map.addMarker(new MarkerOptions().position(coordinates).title(address + "\n" + city + ", " +
-                state + " " + postalCode + "\n" + country + "\n" + knownName));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 17));
+        //concatenate pieces into a full address
+        String fullAddress = (address + ", " + city + ", " + state + " " + postalCode + " " + country + " ");
+
+        //Searching for a specific address will display a pin at that location.
+        //Adding a lat/lng to the intent URI will bias the results towards a particular area:
+        Uri gmmIntentUri = Uri.parse("geo:"+coLat+", "+coLong+"?q="+fullAddress);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
     }
 }
